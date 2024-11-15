93fb5d1c47becfd6ef4a059e825747e064ae377c
Acute-Tri-angle <pptran106@student.hbuhsd.edu>
2024-11-14 19:51:49 -0800

    v1

--- Main.java
+++ Main.java
@@ -5,5 +5,29 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter 2 numbers");
+    int x = sc.nextInt();
+    int y = sc.nextInt();
+    for (int i = x; i <= y; i++){
+      if (i % 2 == 1){
+        System.out.print(i + " ");
+      }
+    }
+
+    System.out.println("");
+
+    System.out.println("enter a positive int");
+    x = sc.nextInt();
+    int xFactor = 10;
+    int currInt;
+    while (x / (xFactor/10) != 0){
+      currInt = x % xFactor;
+      System.out.println(currInt);
+      x -= currInt;
+      xFactor *= 10;
+    }
+
+    sc.close();
   }
 }
